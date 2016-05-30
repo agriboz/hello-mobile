25c02f0a5f844cb16501c1fdf224cd076fc73bf6
angular-cli <angular-cli@angular.io>
2016-05-30 11:20:07 +0300

    chore: initial commit from angular-cli

---
+++ src/app/hello-mobile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'hello-mobile-app',
+  templateUrl: 'hello-mobile.component.html',
+  styleUrls: ['hello-mobile.component.css']
+})
+export class HelloMobileAppComponent {
+  title = 'hello-mobile works!';
+}
